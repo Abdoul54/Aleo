6302b3bfcb65dc9d2ba0258943af8c201d5988ce
zakariaeheddaje <zakariaeheddaje@gmail.com>
2023-07-02 01:48:25 +0100

    Bottons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,163 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import axios from "axios";
-import Landing from "./Landing";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import './App.css';
-const API_BASE_URL = "http://localhost:5000/api";
+import Landing from "./components/Landing";
+import Search from "./components/Search";
 
-function Home() {
-	const [posts, setPosts] = useState([]);
 
-	useEffect(() => {
-		fetchPosts();
-	}, []);
-
-	const fetchPosts = async () => {
-		try {
-			const response = await axios.get(`${API_BASE_URL}/posts`);
-			setPosts(response.data);
-		} catch (error) {
-			console.error("Error fetching posts:", error);
-		}
-	};
-
-	return (
-		<div>
-			<Search setPosts={setPosts} />
-			<div
-				className="card"
-
-			>
-				{posts.map((post) => (
-					<div className="card" key={post._id}>
-						<img
-							src={post.image}
-							className="card-img-top"
-							alt="..."
-							fluid
-							width={300} height={300}
-						/>
-						<div className="card-body">
-							<h4 className="card-title">{post.Title}</h4>
-							<p className="card-text">{post.localisation}</p>
-							<p className="card-text">{post.type}</p>
-							<p className="card-text">{post.Price} DHs</p>
-							<p className="card-text">{post.platform}</p>
-							<a
-								href={post.link}
-								target="_blank"
-								rel="noreferrer"
-								className="btn btn-primary"
-							>
-								Visit Link
-							</a>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
-}
-
-function Search(props) {
-	const [searchQuery, setSearchQuery] = useState("");
-	const [searchResults, setSearchResults] = useState([]);
-	const [searchOption, setSearchOption] = useState("Vehicle");
-
-	const searchPosts = async () => {
-		try {
-			const response = await axios.get(
-				`${API_BASE_URL}/search/${searchOption}/${searchQuery}`
-			);
-			setSearchResults(response.data);
-		} catch (error) {
-			console.error("Error searching posts:", error);
-		}
-	};
-
-	const handleSearchInputChange = (event) => {
-		setSearchQuery(event.target.value);
-	};
-	const handleSearchOptionChange = (event) => {
-		setSearchOption(event.target.value);
-	};
-
-	const handleSearchSubmit = (event) => {
-		event.preventDefault();
-		props.setPosts([]);
-		searchPosts();
-	};
-	return (
-		<div>
-			<Form
-				onSubmit={handleSearchSubmit}
-				className="d-flex"
-			>
-				<div className="d-flex align-items-center" sticky="top">
-					<Form.Control
-						as="select"
-						type="select"
-						value={searchOption}
-						onChange={handleSearchOptionChange}
-						className="me-2"
-					>
-						<option value="Vehicle">Vehicle</option>
-						<option value="Property">Property</option>
-						<option value="Job">Job</option>
-					</Form.Control>
-					<Form.Control
-						type="text"
-						value={searchQuery}
-						onChange={handleSearchInputChange}
-						placeholder="Search..."
-						className="me-2"
-					/>
-					<Button type="submit" variant="outline-success">
-						Search
-					</Button>
-				</div>
-			</Form>
-			<div className="card">
-				{searchResults.map((post) => (
-					<div key={post._id}>
-						<img
-							src={post.image}
-							className="card-img-top"
-							alt="..."
-							fluid
-							width={300} height={300}
-						/>
-						<div className="card-body">
-							<h4 className="card-title">{post.Title}</h4>
-							<p className="card-text">{post.localisation}</p>
-							<p className="card-text">{post.type}</p>
-							<p className="card-text">
-								{post.Price === 0 ? "Not Defined" : post.Price + " DHs"}
-							</p>
-							<p className="card-text">{post.platform}</p>
-							<div className="mt-auto">
-								<a
-									href={post.link}
-									target="_blank"
-									rel="noreferrer"
-									className="btn btn-primary text-center"
-								>
-									Visit Link
-								</a>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
-}
 
 function App() {
 	return (
@@ -176,8 +27,8 @@ function App() {
 								</Link>
 							</Nav.Item>
 							<Nav.Item>
-								<Link to="/home" className="nav-link">
-									Posts
+								<Link to="/search" className="nav-link">
+									Search
 								</Link>
 							</Nav.Item>
 						</Nav>
@@ -188,7 +39,7 @@ function App() {
 			<Container fluid>
 				<Routes>
 					<Route path="/" element={<Landing />} />
-					<Route path="/home" element={<Home />} />
+					<Route path="/search" element={<Search />} />
 				</Routes>
 			</Container>
 		</Router>
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import './Search.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+const API_BASE_URL = "http://localhost:5000/api";
+
+
+function Search() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchOption, setSearchOption] = useState("Vehicle");
+
+    const searchPosts = async () => {
+        try {
+            const response = await axios.get(
+                `${API_BASE_URL}/search/${searchOption}/${searchQuery}`
+            );
+            setSearchResults(response.data);
+        } catch (error) {
+            console.error("Error searching posts:", error);
+        }
+    };
+
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+    const handleSearchOptionChange = (event) => {
+        setSearchOption(event.target.value);
+    };
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
+        searchPosts();
+    };
+    return (
+        <div>
+            <Form
+                onSubmit={handleSearchSubmit}
+                className="d-flex"
+            >
+                <div className="d-flex align-items-center" sticky="top">
+                    <Form.Control
+                        as="select"
+                        type="select"
+                        value={searchOption}
+                        onChange={handleSearchOptionChange}
+                        className="me-2"
+                    >
+                        <option value="Vehicle">Vehicle</option>
+                        <option value="Property">Property</option>
+                        <option value="Job">Job</option>
+                    </Form.Control>
+                    <Form.Control
+                        type="text"
+                        value={searchQuery}
+                        onChange={handleSearchInputChange}
+                        placeholder="Search..."
+                        className="me-2"
+                    />
+                    <Button
+                        type="submit"
+                        variant="outline-success"
+                        className="btn btn-primary btn-lg btn-block btn2">
+                        Search
+                    </Button>
+                </div>
+            </Form>
+            <div className="card">
+                {searchResults.map((post) => (
+                    <div key={post._id}>
+                        <img
+                            src={post.image}
+                            className="card-img-top"
+                            alt="..."
+                            fluid
+                            width={300} height={300}
+                        />
+                        <div className="card-body">
+                            <h4 className="card-title">{post.Title}</h4>
+                            <p className="card-text local">{post.localisation}</p>
+                            <p className="card-text type">{post.type}</p>
+                            <p className="card-text price">
+                                {post.Price === 0 ? "Not Defined" : post.Price + " DHs"}
+                            </p>
+                            <div className="mt-auto">
+                                <Button
+                                    href="https://www.avito.ma"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    variant="outline-primary"
+                                    className="btn btn-primary btn-lg btn-block btn1"
+                                    disabled
+                                >
+                                    {post.platform}
+                                </Button>
+
+                                <Button
+                                    href={post.link}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    variant="outline-success"
+                                    className="btn btn-primary btn-lg btn-block btn2"
+                                >
+                                    Visit Link
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Search;
