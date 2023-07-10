cdbbccfe0e0e05bf54acc137ae052cb8035fac1a
Abdoul54 <abdelwahed.akhechane@gmail.com>
2023-07-11 00:48:26 +0100

    landing page styled

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,59 +1,159 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Header = () => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import "./Landing.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Your Web App. All rights reserved.</p>
+    <footer className="footer bg-light">
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>Subscribe to our newsletter</h4>
+            <p>Stay updated with the latest news and offers.</p>
+          </div>
+          <div className="col-md-6">
+            <form>
+              <div className="input-group">
+                <input type="email" className="form-control" placeholder="Enter your email" />
+                <button className="btn btn-primary" type="submit">Subscribe</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <p className="mt-4">&copy; {new Date().getFullYear()} Aleo. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
 
-const Landing = () => {
+const NumberInfoSection = ({ title, number, description }) => {
+  return (
+    <div className="number-info">
+      <div className="number-info-inner">
+        <span className="number-info-number">{number}</span>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+const ImageTextSection = () => {
+  return (
+    <div className="image-text-section">
+      <div className="image-text-section-content">
+        <div className="image-text-section-image">
+          <img src="/pictures/car.jpg" alt="Vehicle" />
+        </div>
+        <div className="image-text-section-text">
+          <h2>Vehicles</h2>
+          <p>Explore our wide range of vehicles for sale.</p>
+          <Link to="/search" className="nav-link me-2">
+            <button type="button" className="Go">
+              Discover
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="image-text-section-content">
+        <div className="image-text-section-image">
+          <img src="/pictures/house.jpg" alt="property" />
+        </div>
+        <div className="image-text-section-text">
+          <h2>Properties</h2>
+          <p>Discover properties available for sale or rent.</p>
+          <Link to="/search" className="nav-link me-2">
+            <button type="button" className="Go">
+              Discover
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="image-text-section-content">
+        <div className="image-text-section-image">
+          <img src="/pictures/work.jpg" alt="job" />
+        </div>
+        <div className="image-text-section-text">
+          <h2>Jobs</h2>
+          <p>Find exciting job opportunities.</p>
+          <Link to="/search" className="nav-link me-2">
+            <button type="button" className="Go">
+              Discover
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const LandingPage = () => {
   return (
     <div className="landing-page">
-      <Header />
-      <h1>Welcome to the Web App!</h1>
-      <div className="categories">
-        <Link to="/vehicles" className="category">
-          <h2>Vehicles</h2>
-          <img src='../public/pictures/vehicle.jpeg' alt="Vehicle" />
-          <p>Explore our wide range of vehicles for sale.</p>
-        </Link>
-        <Link to="/properties" className="category">
-          <h2>Properties</h2>
-          <img src='/public/pictures/properties.jpeg' alt="Properties" />
-          <p>Discover properties available for sale or rent.</p>
-        </Link>
-        <Link to="/jobs" className="category">
-          <h2>Job Offers</h2>
-          <img src='/public/pictures/job.jpeg' alt="Job" />
-          <p>Find exciting job opportunities.</p>
-        </Link>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{ height: "600px", "object-fit": "cover" }}
+            src="/pictures/job.jpeg"
+            alt="Slider 1"
+          />
+          <Carousel.Caption className="caption">
+            <h3>Job Offers</h3>
+            <p>Find exciting job opportunities.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{ height: "600px", "object-fit": "cover" }}
+            src="/pictures/properties.jpeg"
+            alt="Slider 2"
+          />
+          <Carousel.Caption className="caption">
+            <h3>Properties</h3>
+            <p>Discover properties available for sale or rent.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{ height: "600px", "object-fit": "cover" }}
+            src="/pictures/vehicle.jpeg"
+            alt="Slider 3"
+          />
+          <Carousel.Caption className="caption">
+            <h3>Vehicles</h3>
+            <p>Explore our wide range of vehicles for sale.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <div className="container">
+        <div className="number-info-sections">
+          <div className="number-info-center">
+            <NumberInfoSection
+              title="Total Posts"
+              number="+20,000"
+              description="Explore thousands of posts from various sources."
+            />
+            <NumberInfoSection
+              title="New Posts Today"
+              number="+4,000"
+              description="Discover fresh posts added today and stay up-to-date."
+            />
+            <NumberInfoSection
+              title="Number of Source apps"
+              number="+20"
+              description="Get posts from multiple apps to access a diverse range of content."
+            />
+          </div>
+        </div>
+          <ImageTextSection/>
       </div>
       <Footer />
     </div>
   );
 };
 
-export default Landing;
+export default LandingPage;
