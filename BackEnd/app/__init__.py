e4669a4b94bfa8d2f0da72cbfccc61ca133e9fb5
zakariaeheddaje <zakariaeheddaje@gmail.com>
2023-07-10 16:01:50 +0100

    landing

--- BackEnd/app/__init__.py
+++ BackEnd/app/__init__.py
@@ -4,4 +4,4 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3001'])
 # Import routes module
-from app import routes
+
--- BackEnd/run.py
+++ BackEnd/run.py
@@ -1,5 +1,4 @@
 from app import app
-from flask_socketio 
 
 if __name__ == '__main__':
     app.run(debug=True)
