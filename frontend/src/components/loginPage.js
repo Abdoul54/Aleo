6436daa0bfb9a6046b82854e7a81eed32687e45a
zakariaeheddaje <zakariaeheddaje@gmail.com>
2023-07-11 00:48:35 +0100

    0x08-recursion

--- frontend/src/components/loginPage.js
+++ frontend/src/components/loginPage.js
@@ -44,29 +44,5 @@ const LoginPage = ({ onLogin }) => {
   );
 };
 
-
-const LoginForm = () => {
-  return (
-    <div className="login-form">
-      <h1>Login</h1>
-      <form>
-        <div className="form-group">
-          <label className="username">Username</label>
-          <input type="text" className="form-control" id="username" />
-        </div>
-        <div className="form-group">
-          <label className="password">Password</label>
-          <input type="password"  id="password" />
-        </div>
-        <div className="form-group form-check">
-          <input type="checkbox" className="form-check-input" id="rememberme" />
-          <label className="form-check-label">Remember me</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form >
-    </div >
-  )
-}
-
 export default LoginPage;
 
