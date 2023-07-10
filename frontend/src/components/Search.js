997e43ff6b7b0caceea6fc731261fdc47abd40b3
zakariaeheddaje <zakariaeheddaje@gmail.com>
2023-07-11 00:47:23 +0100

    login & admin pages

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import "./Search.css";
+import "./style/Search.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FiFilter, FiXCircle } from 'react-icons/fi';
 import { GiPositionMarker } from 'react-icons/gi';
--- frontend/src/components/loginPage.js
+++ frontend/src/components/loginPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './style/loginPage.css'
 
 const LoginPage = ({ onLogin }) => {
   const [username, setUsername] = useState('');
@@ -14,29 +15,58 @@ const LoginPage = ({ onLogin }) => {
   };
 
   return (
-    <div>
-      <h2>Admin Login</h2>
+    <div className="login-form">
+      <h1>Admin Login</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <br />
-        <button type="submit">Login</button>
+        <div className='form-group'>
+          <input
+            type="text"
+            placeholder="Username"
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
       </form>
     </div>
   );
 };
 
+
+const LoginForm = () => {
+  return (
+    <div className="login-form">
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label className="username">Username</label>
+          <input type="text" className="form-control" id="username" />
+        </div>
+        <div className="form-group">
+          <label className="password">Password</label>
+          <input type="password"  id="password" />
+        </div>
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="rememberme" />
+          <label className="form-check-label">Remember me</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form >
+    </div >
+  )
+}
+
 export default LoginPage;
+
