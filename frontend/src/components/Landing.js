e8eb0279c2d9384bcd5735604b084eff7f1dcd66
zakariaeheddaje <zakariaeheddaje@gmail.com>
2023-07-12 00:20:38 +0100

    move landing to style

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import "./Landing.css";
+import "./style/Landing.css";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
